e2a1f621ad5b06209c380db25ef94a2b0ef7db64
Epicjeong <yurijeong@icloud.com>
2025-11-07 19:26:12 -0800

    lab-10 upload
    Should be all of lab 10

---
+++ Assignment_1_Import/Assets/Scripts/DuctTape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GD14_1133_DiceGame_Jeong_Yuri.Scripts
+{
+    internal class DuctTape : DuraItem
+    {
+        //Minimum and max durability that can be restored
+        public DuctTape()
+        {
+            minDuraRestored = 4;
+            maxDuraRestored = 12;
+        }
+        
+        //What the item does
+        internal override void ItemAction(Player player, System.Random random, DiceRolls diceroller, Map map)
+        {
+            //Gets a random amount of durability to restore
+            duraRestored = random.Next(minDuraRestored, maxDuraRestored);
+        }
+    }
+}
---
+++ Assignment_1_Import/Assets/Scripts/MagnifyGlass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GD14_1133_DiceGame_Jeong_Yuri.Scripts
+{
+    /// <summary>
+    /// The subclass for the magnifying glass
+    /// Resets a room and runs the code for entering the room
+    /// </summary>
+    internal class MagnifyGlass : UtilItem
+    {
+        //What the item does
+        internal override void ItemAction(Player player, System.Random random, DiceRolls diceroller, Map map)
+        {
+            //Resets code in map because its easier to do room code there
+            //map.ResetRoom(player, rock, random, diceroller, scoreKeep, endGame, map);
+        }
+    }
+}
